d7dd64a4f3b07b5e135f3be106d211201f2c3afa
Matthew Harrilal <matthewharrilal@gmail.com>
2018-03-06 16:05:57 -0800

    Completing the network request and setting the state for the user attributes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Pokemon from './pokemon'
 class App extends Component {
   render() {
     return (
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Pokemon/>
         </p>
       </div>
     );
